4f3c07e69e32ed7962461dc560e4d73b29780334
lakmal-codezync <lakmal@codezync.com>
2026-07-08 10:48:15 +0530

    Fix Playwright CI timeout

--- playwright.config.js
+++ playwright.config.js
@@ -15,6 +15,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests/specs',
   timeout: 60 * 1000,
+  globalTimeout: process.env.CI ? 60 * 60 * 1000 : undefined,
   expect: {
     timeout: 15 * 1000,
   },
@@ -23,7 +24,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
